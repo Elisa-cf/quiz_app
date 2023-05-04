213a45307f4f9ea34509c00b66091c62b8f4715a
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2023-05-04 11:49:03 +0200

    add in a column center the text and the buttons with the answers

--- quizz_app/lib/questions_screen.dart
+++ quizz_app/lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -10,6 +11,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('the question'),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
