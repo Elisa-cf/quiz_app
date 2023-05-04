8ea92cbcf4bc6fbed9b920d062858dae3ce670dc
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2023-05-04 15:35:55 +0200

    mutating values in memory, create a copy of List store it in a variable, call the shuffle method to change the list in placed and the return the updated shuffle list and use it with the map

--- quizz_app/lib/models/quiz_question.dart
+++ quizz_app/lib/models/quiz_question.dart
@@ -2,4 +2,13 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
   final String text;
   final List<String> answers;
+
+  List<String> getShuffleAnswers() {
+    //create a copy of the List and store it in a variable
+    final shuffledList = List.of(answers);
+    //we call shuffle and changes the list in placed
+    shuffledList.shuffle();
+    //and then we return the updated shuffle List
+    return shuffledList;
+  }
 }
