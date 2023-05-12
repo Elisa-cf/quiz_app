d64efb95096b49181f3f68e70ce37a337ac85505
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2023-05-12 15:41:01 +0200

    filter and analyizing the list showing how many anwers are correct from the total answers

--- quizz_app/lib/result_screen.dart
+++ quizz_app/lib/result_screen.dart
@@ -23,6 +23,12 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    //FILTER WITH: .where method it doesn't change the original list but returns a new list.
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -30,11 +36,12 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly'),
+            Text(
+                'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly'),
             const SizedBox(
               height: 30,
             ),
-            QuestionsSummary(getSummaryData()),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
